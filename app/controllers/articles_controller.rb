ddafe192f3493aebbbef9f82318d91c845306063
Bob Casazza <rcasazza@gmail.com>
2020-08-26 10:46:24 -0400

    create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #    render plain: @article.inspect
+    @article.save
+    redirect_to @article
+  end
+
 end
